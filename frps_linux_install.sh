6fc848c1960c3c159b0340dd66363b207fcf27fe
LDBZnazimay <mrshenming@outlook.com>
2025-08-28 15:08:34 +0800

    Update frps_linux_install.sh

--- frps_linux_install.sh
+++ frps_linux_install.sh
@@ -12,8 +12,8 @@ Font="\033[0m"
 # fonts color
 
 # variable
-FRP_VERSION=0.62.1
-REPO=stilleshan/frps
+FRP_VERSION=0.64.0
+REPO=LDBZnazimay/frps
 WORK_PATH=$(dirname $(readlink -f $0))
 FRP_NAME=frps
 FRP_PATH=/usr/local/frp
